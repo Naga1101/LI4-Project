315ff44d5a2102a26898676f174dec941899bf09
Rui <ruipedrocerqueira@gmail.com>
2024-01-15 22:51:55 +0000

    small upd

--- JBleiloes/DB/DBConfig.cs
+++ JBleiloes/DB/DBConfig.cs
@@ -7,10 +7,6 @@ namespace JBleiloes.DB
 {
     public class DBConfig
     {
-        public const string USER = "LAPTOP-GRIEQPR6";
-        public const string MACHINE = "LAPTOP-GRIEQPR6\\SQLEXPRESS";
-        public const string DATABASE = "jbleiloes";
-
         public static string Connection()
         {
             return "Data Source =.; Initial Catalog = jbleiloes; Integrated Security = True; Encrypt = False";
