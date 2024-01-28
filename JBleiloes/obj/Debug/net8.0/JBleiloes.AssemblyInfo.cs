ce59fe66fe2e46247e273df146315357fd3a5a6b
Rui <ruipedrocerqueira@gmail.com>
2024-01-28 19:04:36 +0000

    remover procura de admin

--- JBleiloes/obj/Debug/net8.0/JBleiloes.AssemblyInfo.cs
+++ JBleiloes/obj/Debug/net8.0/JBleiloes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JBleiloes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+293e0a1777d0c8583757fd2057fed61b74ce81e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+917a377c5983ad880d329bc7d64e1059596e11da")]
 [assembly: System.Reflection.AssemblyProductAttribute("JBleiloes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JBleiloes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
